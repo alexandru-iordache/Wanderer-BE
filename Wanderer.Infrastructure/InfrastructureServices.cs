6b196f26477a848c7f27623fff8d2bb2c24c5a96
alexcojk2002 <iordache.alexandru2002@gmail.com>
2023-08-09 19:35:44 +0300

    [FEATURE] Add Generic Repository

--- Wanderer.Infrastructure/InfrastructureServices.cs
+++ Wanderer.Infrastructure/InfrastructureServices.cs
@@ -2,16 +2,19 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Wanderer.Infrastructure.Context;
+using Wanderer.Infrastructure.Repositories.Generics;
 
 namespace Wanderer.Infrastructure;
 
 public static class InfrastructureServices
-{ 
+{
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<WandererDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("WandererDBConnection")), 
-            ServiceLifetime.Singleton);
+        services.AddDbContext<WandererDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("WandererDBConnection")),
+                             ServiceLifetime.Singleton);
 
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         return services;
     }
 }
